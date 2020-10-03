1005e7c00670a288c14ed681ccd4141869a685ad
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-10-03 02:44:19 -0500

    Kinda integrates store and ws handler? Toasts are broken though 😢

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom';
 import store from './redux/create-store';
 import { Provider } from 'react-redux';
 import HomeComponent from './components/home';
-import wsHandler from './middleware/ws-client';
+import { ToastContainer } from 'react-toastify';
+import WSHandler from './middleware/ws-client';
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
-
+const wsHandler = new WSHandler(store);
+wsHandler.initialize();
 const WebSocketContext = React.createContext(wsHandler);
 
 class App extends React.Component {
@@ -17,6 +19,7 @@ class App extends React.Component {
                 <Provider store={store}>
                     <p>:D</p>
                     <HomeComponent />
+                    <ToastContainer />
                 </Provider>
             </WebSocketContext.Provider>
         );
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class HomeComponent extends React.Component {
     render() {
-        return <p>:D</p>;
+        return <>AH SEND HELP</>;
     }
 }
 
