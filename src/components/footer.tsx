f797b6fcba6456ce32758ced8015207d2e41c947
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-10-04 15:33:41 -0500

    First pass at adding a new font for the home screen

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Content } from 'bloomer/lib/elements/Content';
 import { Container } from 'bloomer/lib/layout/Container';
 import { Footer } from 'bloomer/lib/layout/Footer';
 import React from 'react';
-import './footer.scss';
+import '../styles/footer.scss';
 
 class FooterComponent extends React.Component {
     render() {
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,14 +1,16 @@
 import { Container } from 'bloomer/lib/layout/Container';
 import React from 'react';
-import './home.scss';
+import '../styles/home.scss';
 
 class HomeComponent extends React.Component {
     render() {
         return (
             <Container className="home-wrapper">
                 <div className="title-wrapper">
-                    <span>HotS Medallion</span>
-                    <span>Tracker</span>
+                    <div className="title">
+                        <span className="text large">HotS Medallion</span>
+                        <span className="text xlarge">Tracker</span>
+                    </div>
                 </div>
                 <div className="form-wrapper">BattleTag/Name</div>
             </Container>
