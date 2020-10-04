accc6b71c834e1c8dcd34bed605ae096a84bfcb6
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-10-04 18:29:19 -0500

    home component tweaks

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,21 +1,73 @@
 import { Container } from 'bloomer/lib/layout/Container';
 import React from 'react';
+import WSHandler from '../middleware/ws-client';
 import '../styles/home.scss';
 
-class HomeComponent extends React.Component {
+interface HomeState {
+    userName: string;
+}
+// eslint-disable-next-line @typescript-eslint/ban-types
+class HomeComponent extends React.Component<{}, HomeState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+        };
+
+        this.handleUserNameChange = this.handleUserNameChange.bind(this);
+        this.isUserNameValid = this.isUserNameValid.bind(this);
+        this.joinRoom = this.joinRoom.bind(this);
+        this.createRoom = this.createRoom.bind(this);
+    }
+
+    handleUserNameChange(event) {
+        // TODO: Validations and whatnot
+        this.setState({ userName: event?.target?.value });
+    }
+
+    joinRoom() {
+        console.log(this.context);
+    }
+
+    createRoom() {
+        // TODO
+    }
+
+    isUserNameValid(userName?: string): boolean {
+        let name = userName;
+        if (!name) {
+            name = this.state.userName;
+        }
+        return name.length > 0;
+    }
+
     render() {
         return (
             <Container className="home-wrapper">
-                <div className="title-wrapper">
+                <div className="section-wrapper">
                     <div className="title">
-                        <span className="text large">HotS Medallion</span>
+                        <span className="text large">HotS Medallion</span> <br />
                         <span className="text xlarge">Tracker</span>
                     </div>
                 </div>
-                <div className="form-wrapper">BattleTag/Name</div>
+                <div className="section-wrapper form-wrapper">
+                    <span className="text small">BattleTag / Name</span>
+                    <input type="text" value={this.state.userName} onChange={this.handleUserNameChange} />
+                    <input
+                        type="button"
+                        value="Create Room"
+                        disabled={!this.isUserNameValid.call(this)}
+                        onClick={this.createRoom}
+                    />
+                    <input
+                        type="button"
+                        value="Join Room"
+                        disabled={!this.isUserNameValid.call(this)}
+                        onClick={this.joinRoom}
+                    />
+                </div>
             </Container>
         );
     }
 }
-
 export default HomeComponent;
