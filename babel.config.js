22fd1e045033809e24bc39a1a5e3e2e575743bff
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-10-05 23:55:42 -0500

    Weird babel shit to support decorators? Still broken though

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: [
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true,
+            },
+        ],
+        '@babel/plugin-proposal-class-properties',
+    ],
 };
