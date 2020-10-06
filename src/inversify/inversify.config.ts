11ddcdaa1d818a5191040dc32dd41398292e170b
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-10-05 23:56:10 -0500

    First draft at inversify config

---
+++ src/inversify/inversify.config.ts
@@ -0,0 +1,17 @@
+import { Container } from 'inversify';
+import 'reflect-metadata';
+import { Store } from 'redux';
+import WSHandler, { WSHandlerI } from '../middleware/ws-client';
+import store from '../redux/create-store';
+import { TYPES } from './container-types';
+
+const appContainer = new Container();
+
+appContainer.bind<Store>(TYPES.ReduxStore).toConstantValue(store);
+
+const wsHandler = new WSHandler(store);
+wsHandler.initialize();
+
+appContainer.bind<WSHandlerI>(TYPES.WsHandler).toConstantValue(wsHandler);
+
+export { appContainer };
